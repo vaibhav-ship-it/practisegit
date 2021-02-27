409de379909590e84d97cbbb98edea8056f04eb8
Vaibhav <vaibhav0320@gmail.com>
2021-02-27 07:24:26 +0530

    Commit using SSH

---
+++ practise/src/practisegit/A.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package practisegit;
+
+/**
+ * @author vaibhav
+ *
+ */
+public class A {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
