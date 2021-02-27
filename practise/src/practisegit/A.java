701d68415ae537ef66e5f3250d0a851d91aa6923
Vaibhav <vaibhav0320@gmail.com>
2021-02-27 08:52:33 +0530

    A.java committed in dev branch

--- practise/src/practisegit/A.java
+++ practise/src/practisegit/A.java
@@ -13,8 +13,10 @@ public class A {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+	  // Comment added for testing	
 	  System.out.println("Inside A.java main method");
 	  System.out.println("Another line added for testing purpose");
+	  System.out.println("Another line");
 	}
 
 }
