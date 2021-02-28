ceab377e34d0da6d1f6d3aa9e7d58b6a26e009d9
Vaibhav <75160383+vaibhav-ship-it@users.noreply.github.com>
2021-02-28 13:48:54 +0530

    Change from feature

--- practise/src/practisegit/Employee.java
+++ practise/src/practisegit/Employee.java
@@ -1,11 +1,12 @@
 package practisegit;
 // Employee class
 public class Employee {
+	//Change 2
 	private String name;
 	private String state;
 	private String address; // Conflicts resolved
 	private long salary;	
 	private int age;
-
+	//Just a comment!
 }
 
